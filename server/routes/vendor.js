9d6e8c35dc107b2ea72c2799e1c56c189b939014
Rohan Doshi <rohan.doshi02@gmail.com>
2022-10-30 14:58:19 +0530

    Vendor Orders view API

--- server/routes/vendor.js
+++ server/routes/vendor.js
@@ -87,4 +87,21 @@ vendorRouter.get("/me", isAuthenticated, (req, res) => {
   res.send({ user: req.user });
 });
 
+vendorRouter.get("/myOrders", isAuthenticated, async (req, res) => {
+    let text =
+		"select Cart.quantity, Items.name, Items.selling_price, Items.mrp, Customers.full_name, Customers.mobile_number, Customers.email, CONCAT(Address.line_1, ' ', Address.line_2, ' ', Address.city, ' ', Address.user_state, ' ', Address.postal_code) as Address from Cart inner join Customer_Order on Cart.fk_order=Customer_Order.order_id inner join Customers on Customer_Order.fk_customer = Customers.id inner join Items on Cart.fk_item=Items.id inner join Customer_Address on Customers.id=Customer_Address.customer_id inner join Address on Customer_Address.address_id=Address.id where Items.fk_vendor=$1 and Customer_Order.status='CONFIRMED'";
+    let values = [req.user.id];
+    try {
+        const data = await client.query(text, values);
+        if (data.rowCount === 0) {
+            return res.status(404).json({ error: "No orders Found" });
+        } else {
+            return res.json({ orders: data.rows });
+        }
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: "Internal Server Error." });
+    }
+});
+
 module.exports = vendorRouter;
